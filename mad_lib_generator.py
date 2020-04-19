c6983df8ba318ad81d4e7ee7bd674d15ab9306c0
Jay <34511691+jhobaugh@users.noreply.github.com>
2020-04-19 11:03:05 -0400

    Add files via upload

---
+++ mad_lib_generator.py
@@ -0,0 +1,81 @@
+
+# adlib template
+'''One very nice morning near the end of summer,
+my mother woke me up at 4:00 A.M. and said, "Wake
+up and smell the grass, sleepy head! Today is your
+first day of school and you can't be late." I groaned in
+my bed for twenty seconds, but eventually I got
+dressed. I wore a blue and white striped, long sleeve
+_______________(noun) with a collar on it, a red tie,
+dark gray pants, white socks, black shoes, and a(n)
+_______________(adjective) hat. In ten minutes I made
+lunch and ate my breakfast. _______________(number)
+minutes later, the bus came. A few minutes later, I
+was at school.
+In school, I met two really
+_______________(adjective) kids. All of us became
+friends very fast. That day we had Science, and
+luckily my friends and I were at the same
+_______________(noun) .My friends' names are
+_______________(proper noun) and
+_______________(proper noun). In Math we weren't
+together, and that really bugged me. We learned what
+_______________(plural noun) were, and when to use
+them. At snack and recess, we played a game
+together. It was extremely fun. At P. E., we were
+____________(-ing verb) off of the ropes onto
+_______________(plural noun). I thought it was a very
+_______________(adjective) idea. In swimming class,
+we needed to swim extremely
+_______________(adverb), or else we would have to
+swim longer.
+Before I knew it, school was over. I grabbed all
+my belongings and put them into my backpack. In two
+minutes, the bus came. As I stepped into the bus I
+shouted, "Goodbye, adios amigos, and shalom," to
+my friends. Then I went into the bus. In a flash, I was
+back home. This day was an extremely exciting day!'''
+
+# 2 nouns 3 adjectives 1 number 2 proper nouns 2 plural nouns 1 -ing verb 1 adverb
+
+noun1 = str(input("Enter a Noun: "))
+noun2 = str(input("Enter a 2nd Noun: "))
+adj1 = str(input("Enter an Adjective: "))
+adj2 = str(input("Enter a 2nd Adjective: "))
+adj3 = str(input("Enter a 3rd Adjective: "))
+num1 = int(input("Enter a Number: "))
+pr_noun1 = str(input("Enter a Proper Noun: "))
+pr_noun2 = str(input("Enter another Proper Noun: "))
+pl_noun1 = str(input("Enter a Plural Noun: "))
+pl_noun2 = str(input("Enter another Plural Noun: "))
+
+verb = str(input("Enter a -ing verb: "))
+adverb = str(input("Enter a adverb: "))
+
+
+print("One very nice morning near the end of summer, my mother woke me up at 4:00 A.M. and said,\n",
+"Wake up and smell the grass, sleepy head! Today is your first day of school and you can't be late.\n",
+" I groaned in my bed for twenty seconds, but eventually I got dressed.\n",
+"I wore a blue and white striped, long sleeve\n",
+noun1, " with a collar on it, a red tie,\n",
+"dark gray pants, white socks, black shoes, and a(n)\n",
+adj1, " hat. In ten minutes I made\n",
+"lunch and ate my breakfast. ", num1,
+"\n minutes later, the bus came. A few minutes later, I\n",
+"was at school. In school, I met two really\n",
+adj2, "kids. All of us became\n",
+"friends very fast. That day we had Science, and\n",
+"luckily my friends and I were at the same\n",
+noun2, ".My friends' names are ", pr_noun1, " and ", pr_noun2,
+"In Math we weren't together, and that really bugged me. We learned what\n",
+pl_noun1, "were, and when to use them. At snack and recess, we played a game\n",
+"together. It was extremely fun. At P. E., we were ", verb,
+"\n off of the ropes onto", pl_noun2, ". I thought it was a very\n",
+adj3, "idea. In swimming class, we needed to swim extremely\n",
+adverb, ", or else we would have to swim longer.",
+"Before I knew it, school was over. I grabbed all my belongings and put them into my backpack. In two\n",
+"minutes, the bus came. As I stepped into the bus I\n",
+"shouted, \"Goodbye, adios amigos, and shalom,\" to my friends. Then I went into the bus. In a flash, I was\n",
+"back home. This day was an extremely exciting day!")
+
+
